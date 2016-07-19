b62ff4f8306dbdb80b0bf6ccbb87f07e19e8af8e
Johan AR <j3valentin@gmail.com>
2016-07-19 14:21:19 -0500

    Primer confirmacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import {Component, ViewEncapsulation} from 'angular2/core';
 import {ROUTER_DIRECTIVES, RouteConfig} from 'angular2/router';
 import {NavbarComponent} from './navbar.component';
-import {HomeComponent} from '../../home/components/home.component';
-import {AboutComponent} from '../../about/components/about.component';
-import {NameListService} from '../../shared/services/name-list.service';
+import {HomeComponent} from './home/home.component';
+import {AboutComponent} from './about/about.component';
+import {NameListService} from '../shared/services/name-list.service';
 
 @Component({
   selector: 'sd-app',
