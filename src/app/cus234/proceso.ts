b086e9379b1190240a99dce446fdd6dae9a9d6e6
Johan AR <j3valentin@gmail.com>
2016-08-11 20:35:53 -0500

    Contrato de definición de Proceso a interfaz para propiedades opcionales refs #989 @1.5h

--- src/app/cus234/proceso.ts
+++ src/app/cus234/proceso.ts
@@ -1,42 +1,41 @@
-import {Juzgado}        from './juzgado';
-import {Apoderado}        from './apoderado';
-import {Afiliado}        from './afiliado';
-export class Proceso {
-    constructor(
-        public notificacion: string,
-        public numero: number,
-        public bizagi: string,
-        public tipo: string,
-        public demandate: string,
-        public cedula: number,
-        public despacho: string,
-        public estado: string,
-        public id?: number,
-        public litigar?: number,
-        public fecha?: string,
-        public juzgado?: Juzgado,
-        public numRadi?: string,
-        public clasificacion?: string,
-        public demandado?: string,
-        public apoderado?: Apoderado,
-        public clasTramites?: string,
-        public tipoInf?: string,
-        public relacion?: string,
-        public regional?: string,
-        public anioRadi?: string,
-        public tipoUni?: string,
-        public hechos?: string,
-        public actoAdmin?: string,
-        public numRes?: string,
-        public causa?: string,
-        public modPret?: string,
-        public pretencion?: string,
-        public clasePret?: string,
-        public fechaDem?: string,
-        public cuantia?: string,
-        public afiliado?: Afiliado,
-        public canDemandantes?: number,
-        public respuesta?: string,
-        public apodeContra?: Apoderado
-    ) { }
+import {Juzgado}   from './juzgado';
+import {Apoderado} from './apoderado';
+import {Afiliado}  from './afiliado';
+
+export interface Proceso {
+  notificacion: string;
+  numero: number;
+  bizagi: string;
+  tipo: string;
+  demandate: string;
+  cedula: number;
+  despacho: string;
+  estado: string;
+  id?: number;
+  litigar?: number;
+  fecha?: string;
+  juzgado?: Juzgado;
+  numRadi?: string;
+  clasificacion?: string;
+  demandado?: string;
+  apoderado?: Apoderado;
+  clasTramites?: string;
+  tipoInf?: string;
+  relacion?: string;
+  regional?: string;
+  anioRadi?: string;
+  tipoUni?: string;
+  hechos?: string;
+  actoAdmin?: string;
+  numRes?: string;
+  causa?: string;
+  modPret?: string;
+  pretencion?: string;
+  clasePret?: string;
+  fechaDem?: string;
+  cuantia?: string;
+  afiliado?: Afiliado;
+  canDemandantes?: number;
+  respuesta?: string;
+  apodeContra?: Apoderado;
 }
