8cc378dc532d0255ed7f9a512edfd1ef0b7ff48d
Johan AR <j3valentin@gmail.com>
2016-08-27 18:09:39 -0500

    Registrar Datos Generales - Cargar datos formulario refs #985 @1h Actualización de las opciones de municipios para un departamento seleccionado

--- src/app/cus234/despacho.ts
+++ src/app/cus234/despacho.ts
@@ -2,7 +2,7 @@ export interface Despacho {
   dei_id: number,
   dei_descripcion?: string,
   tij_id?: string,
-  dpt_id?: number,
+  dpt_id?: string,
   mpi_id?: number,
   dei_consecutivoid?: string,
   dei_concatenado?: string
